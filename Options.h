116b6b270b5c23b208f5947f9fc47e6361e10401
danomatika <danomatika@gmail.com>
2016-11-11 18:12:48 -0700

    updated tlmc and removed extra line in Options help

--- Options.h
+++ Options.h
@@ -11,7 +11,7 @@
 
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 	GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public License
@@ -402,7 +402,7 @@ class Options {
 		/// note: strings must be const (literal)
 		void addArgument(const char *name, const char *help, bool required=false) {
 			if(nonOptions.empty()) { // add header
-				addHelp("\nArguments:\n");
+				addHelp("\nArguments:");
 			}
 			addHelp(help);
 			struct NonOption arg = {name, required};
